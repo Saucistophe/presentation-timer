d1f75fdd9e69ac19c6114e3d16af0ce07ba866dc
Christophe <carpentier.ch@gmail.com>
2025-02-27 23:11:37 +0100

    Tweaked adapter: oopsie

--- svelte.config.js
+++ svelte.config.js
@@ -9,8 +9,6 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
@@ -21,17 +19,3 @@ const config = {
 };
 
 export default config;
-
-export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
-};
